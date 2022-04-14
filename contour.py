99705658bd9d0e6e0d1951de22cf9ea379e3ad80
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-14 23:33:14 +0200

    Contour Detection and Drawing it on an image

--- contour.py
+++ contour.py
@@ -1,8 +1,13 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('Photos/cats.jpg')
 
 cv.imshow("Cats", img)
+
+# createing a blank image
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow("Blank Image", blank)
 
 grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow("Grey", grey)
@@ -16,10 +21,17 @@ cv.imshow("Blurring an Image", blur)
 canny = cv.Canny(blur, 125, 175)
 cv.imshow("Canny Edges", canny)
 
+# USing Threshold
+# ret, thresh = cv.threshold(grey, 125, 255, cv.THRESH_BINARY)
+# cv.imshow("Threshold Image", thresh)
 # Finding the contours
 
 contours, hierachies = cv.findContours(
     canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
 print(f'{len(contours)} contours in the image')
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 2)
+cv.imshow("COntour Drawn Blank Image", blank)
+
 cv.waitKey(0)
