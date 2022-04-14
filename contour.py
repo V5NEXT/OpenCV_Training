4c5fe45fc90c90765071c75244c591a4b7312d9c
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-14 23:14:21 +0200

    Contour Detection Intial Commit

--- contour.py
+++ contour.py
@@ -4,5 +4,22 @@ img = cv.imread('Photos/cats.jpg')
 
 cv.imshow("Cats", img)
 
+grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Grey", grey)
 
+# Blurring the image
+blur = cv.GaussianBlur(grey, (5, 5), cv.BORDER_DEFAULT)
+cv.imshow("Blurring an Image", blur)
+
+#edges or canny
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow("Canny Edges", canny)
+
+# Finding the contours
+
+contours, hierachies = cv.findContours(
+    canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+print(f'{len(contours)} contours in the image')
 cv.waitKey(0)
