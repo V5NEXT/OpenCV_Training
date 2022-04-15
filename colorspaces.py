4b6aefc0a993437bc9f8546b5901aeb539f4fe2f
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-15 17:45:58 +0200

    Color Spaces converstion

--- colorspaces.py
+++ colorspaces.py
@@ -1,7 +1,26 @@
 import cv2 as cv
 
-img = cv.imread("Photos/park.jpg")
+img = cv.imread("Photos/lady.jpg")
 
 cv.imshow("City", img)
+
+# BGE to Grey Scale
+
+grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gray Scale", grey)
+
+# BGR to HSV
+
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow("HSV", hsv)
+
+# BGR to LAB
+
+lab = cv.cvtColor(img, cv.COLOR_RGB2Lab)
+cv.imshow("LAB", lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow("RGB", rgb)
 
 cv.waitKey(0)
