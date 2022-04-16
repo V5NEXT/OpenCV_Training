9a33376f0172eacab44a62ae4152dad2c68a97b1
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-16 15:52:31 +0200

    Different types of blurring

--- bluringandsmoothing.py
+++ bluringandsmoothing.py
@@ -5,5 +5,24 @@ img = cv.imread("Photos/cats.jpg")
 
 cv.imshow("Cats", img)
 
+# Averegaing Blur
+
+average = cv.blur(img, (3, 3))
+cv.imshow("Average Blur", average)
+
+
+# Guassian Blur
+
+guass = cv.GaussianBlur(img, (3, 3), 0)
+cv.imshow("Guassian Blur", guass)
+
+# Median Blur
+median = cv.medianBlur(img, 3)
+cv.imshow("Median", median)
+
+# Bilateral Blur
+bilateral = cv.bilateralFilter(img, 5, 15, 15)
+cv.imshow("Bilateral", bilateral)
+
 
 cv.waitKey(0)
