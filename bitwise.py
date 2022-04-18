516c9cda05889ac62e352b89443759c3cdcf04f3
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-18 14:59:01 +0200

    BITwise operations

--- bitwise.py
+++ bitwise.py
@@ -1,4 +1,6 @@
+from cmath import rect
 import cv2 as cv
+from cv2 import bitwise_not
 import numpy as np
 
 
@@ -8,5 +10,22 @@ circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
 
 cv.imshow("Rectangle", rectangle)
 cv.imshow("Circle", circle)
+
+
+# bitwise_and = cv.bitwise_and(rectangle, circle)
+# cv.imshow("bitwise and", bitwise_and)
+
+# bitwise_or = cv.bitwise_or(rectangle, circle)
+# cv.imshow("bitwise Or", bitwise_or)
+
+# bitwise_no = cv.bitwise_not(rectangle, circle)
+# cv.imshow("bitwise not", bitwise_no)
+
+# good for non intesection regions
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+
+
+cv.imshow("bitwise xor", bitwise_xor)
+
 
 cv.waitKey(0)
