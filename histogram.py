b61c9443b25affed8da4a08d9f472101ce9034bf
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-20 23:08:20 +0200

    Ploted hitogram for BGR images and masks

--- histogram.py
+++ histogram.py
@@ -1,22 +1,53 @@
+from turtle import color
 import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
 
 img = cv.imread("Photos/cats.jpg")
 
 
 cv.imshow("Cats", img)
+blank = np.zeros(img.shape[:2], dtype='uint8')
+# circle = cv.circle(
+#     blank.copy(), (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow("Gray", gray)
+
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow("Gray", gray)
+
+# mask = cv.bitwise_and(gray, gray, mask=circle)
+# cv.imshow("Mask", mask)
 
 # created grayscale histograms
-gray_histogram = cv.calcHist([gray], [0], None, [256], [0, 256])
+
+# gray_histogram = cv.calcHist([gray], [0], mask, [256], [0, 256])
+# plt.figure()
+# plt.title("Gray Scale Histogram")
+# plt.xlabel("Bins")
+# plt.ylabel("# of pixels")
+# plt.plot(gray_histogram)
+# plt.xlim([0, 256])
+# plt.show()
+
+
+# color images
+
+mask = cv.circle(
+    blank.copy(), (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow("Mask", masked)
+
+
 plt.figure()
 plt.title("Gray Scale Histogram")
 plt.xlabel("Bins")
 plt.ylabel("# of pixels")
-plt.plot(gray_histogram)
-plt.xlim([0, 256])
+colors = ('b', 'g', 'r')
+for i, col in enumerate(colors):
+    hist = cv.calcHist([img], [i], mask, [256], [0, 256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
+
 plt.show()
 
 cv.waitKey(0)
