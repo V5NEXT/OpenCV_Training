0d46b3915b941ba35c1c81ac6b727744e0d4d0a8
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-20 23:37:01 +0200

    Adaptive Thresholding

--- thresholding.py
+++ thresholding.py
@@ -13,4 +13,14 @@ cv.imshow("Thresh", thresh)
 threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
 cv.imshow("Thresh", thresh_inv)
 
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C,
+                                       cv.THRESH_BINARY, 11, 3)
+
+cv.imshow("Adaptive Threshold", adaptive_thresh)
+
+
+adaptive_thresh_inv = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C,
+                                           cv.THRESH_BINARY_INV, 11, 3)
+
+cv.imshow("Adaptive Threshold INV", adaptive_thresh_inv)
 cv.waitKey(0)
