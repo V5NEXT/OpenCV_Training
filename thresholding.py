0a4e72782e736cb663026945ae935fb90dc7e7b9
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-20 23:27:21 +0200

    Simple Thresholding

--- thresholding.py
+++ thresholding.py
@@ -4,5 +4,13 @@ img = cv.imread("Photos/cats.jpg")
 
 cv.imshow("Cats", img)
 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Grey", gray)
+
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow("Thresh", thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow("Thresh", thresh_inv)
 
 cv.waitKey(0)
