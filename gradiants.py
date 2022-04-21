9fa78dd0125aa858d53391f0708493a6eecf18ad
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-21 20:54:07 +0200

    Sobel and Canny Edge Detection

--- gradiants.py
+++ gradiants.py
@@ -13,4 +13,15 @@ lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow("Laplacian", lap)
 
+# Sobel
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combined_sobel = cv.bitwise_or(sobelx, sobely)
+
+cv.imshow("Sobel COmbined", combined_sobel)
+
+# canny
+canny = cv.Canny(gray, 150, 175)
+cv.imshow("Canny", canny)
+
 cv.waitKey(0)
