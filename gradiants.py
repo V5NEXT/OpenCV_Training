8623e58f6b6adf1a015acd1e3d3d328ffbe47b49
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-04-21 20:40:54 +0200

    Laplacian Ege detection

--- gradiants.py
+++ gradiants.py
@@ -1,7 +1,16 @@
 import cv2 as cv
+import numpy as np
 
-img = cv.imread("Photos/cats.jpg")
+img = cv.imread("Photos/park.jpg")
 
 cv.imshow("Cats", img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("gray", gray)
+
+# laplacian
+lap = cv.Laplacian(gray, cv.CV_64F)
+lap = np.uint8(np.absolute(lap))
+cv.imshow("Laplacian", lap)
 
 cv.waitKey(0)
